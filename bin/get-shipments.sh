4d0f04e39c1713fd6fbd5f07a399efadfd5f7815
Mike Norton <nortonmd@icloud.com>
2023-09-22 14:48:04 -0500

    initial commit

---
+++ bin/get-shipments.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+curl "https://api.mockaroo.com/api/6189c260?count=1000&key=18e93e20" > "data/ShipmentDetails-$$.csv"
---
+++ bin/get-temperature-readings.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl "https://api.mockaroo.com/api/486bedb0?count=1000&key=18e93e20" > "data/TemperatureReading-$$.csv"
+
